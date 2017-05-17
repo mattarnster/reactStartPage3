89e510d4a5ae39f113de5ac2a7a1aa49b76583dc
Matt Arnold <matt@mattarnster.co.uk>
2017-05-17 19:28:41 +0100

    Modifying no sites message.

--- src/components/NoSites.js
+++ src/components/NoSites.js
@@ -4,23 +4,13 @@ class NoSites extends PureComponent {
     render() {
         return (
             <div className="s12 m12">
-                <div className="card cyan lighten-1">
-                    <div className="card-content">
-                        <span className="card-title white-text darken-4 center"
-                              style={{ display: "block" }}><strong>Welcome to reactStartPage<sup>3</sup></strong></span>
-                        <p className="white-text center">Hi there. reactStartPage<sup>3</sup> is currently being rebuilt.</p>
-                        <br/>
-                        <p className="white-text center">You can create an array of objects and place them in localStorage under the key 'sites' for the time being, just follow the format below:</p>
-                        <div className="card-panel teal white-text">
-                            <pre>[
-{`
-    {
-        "name": 'My Website',
-        "url": 'http://www.mattarnster.co.uk'
-    }
-`}
-]</pre>
-                        </div>
+                <div className="card cyan">
+                    <div className="card-content white-text">
+                        <span className="card-title center"
+                              style={{
+                                  display: 'block'
+                              }}>Welcome to reactStartPage<sup>3</sup></span>
+                        <p className="center">Click 'Add/Remove' in the top-left to get started!</p>
                     </div>
                 </div>
             </div>
