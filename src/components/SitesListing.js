df8988d4efa16e76414ca6a3041aa4a79204c76e
Matt Arnold <mattarnster.co.uk@gmail.com>
2019-10-29 14:11:52 -0400

    Attempt at getting sortable sites - doesn't work

--- src/components/SitesListing.js
+++ src/components/SitesListing.js
@@ -2,10 +2,39 @@ import React, { PureComponent } from 'react'
 
 import { connect } from 'react-redux'
 
+import { replaceSites } from '../actions/actionCreators'
+
+import { SortableContainer, SortableElement } from 'react-sortable-hoc'
+import arrayMove from 'array-move'
+
 import Site from './Site'
 import NoSites from './NoSites'
 
+const SortableItem = SortableElement(({ index, value }) => {
+    return value
+})
+
+const SortableList = SortableContainer(({ items }) => {
+    return (
+        items.map((item, index) => {
+            return <SortableItem key={`${item.key}`} index={index} value={item} />
+        })
+    )
+})
+
 class SitesListing extends PureComponent {
+
+    getSites() {
+        return this.props.sites.map(
+            (site, index) => { 
+                return <Site key={ index } site={ site } color={ this.props.color } textColor={ this.props.textColor } />
+            })
+    }
+
+    onSortEnd = ({oldIndex, newIndex}) => {
+        this.props.dispatch(replaceSites(arrayMove(this.props.sites, oldIndex, newIndex)))
+    };
+
     render() {
         return (
             <div className="sites row"
@@ -15,7 +44,7 @@ class SitesListing extends PureComponent {
                 <div className="col s12 m12"
                      style={{ marginTop: 30 }}>
                     { 
-                        this.props.sites.length > 0 ? this.props.sites.map((site, index) => { return <Site key={ index } site={ site } color={ this.props.color } textColor={ this.props.textColor } /> } ) : <NoSites color={ this.props.color } textColor={ this.props.textColor } />
+                        <SortableList items={ this.getSites() } axis={'xy'} onSortEnd={ this.onSortEnd } />
                     }
                 </div>
             </div>
