05f622d0d6f6141c3015f5aae6b2905dda40ff55
Matt Arnold <mattarnster.co.uk@gmail.com>
2019-10-29 08:33:55 -0400

    Fixing redux dev-tools bug where the app wouldn't load if you didn't have it installed

--- src/store.js
+++ src/store.js
@@ -44,7 +44,9 @@ const store = createStore(
   defaultState,
   compose(
     applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+      : f => f
   )
 );
 
