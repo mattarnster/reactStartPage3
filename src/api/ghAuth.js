f94f82a00ef903821672a54fa9fc851313190a48
Matt Arnold <mattarnster.co.uk@gmail.com>
2020-03-16 15:38:00 -0400

    Change github auth link

--- src/api/ghAuth.js
+++ src/api/ghAuth.js
@@ -1,6 +1,6 @@
 export function ghAuth(code) {
     return new Promise((resolve, reject) => {
-        fetch('https://react.ngstartpage.uk/gh-auth.php?code=' + code,{
+        fetch('https://ngstartpage.uk/gh-auth.php?code=' + code,{
             method: 'POST'
         }).then(data => {
             return data.text()
