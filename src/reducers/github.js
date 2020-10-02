831f49b5f217af608a52b600149effe57740b807
Matt Arnold <mattarnster.co.uk@gmail.com>
2020-10-02 15:22:29 -0400

    Fix formatting

--- src/reducers/github.js
+++ src/reducers/github.js
@@ -6,43 +6,43 @@ var lsh = new localStorageHelper()
 
 const github = (state = {}, action) => {
 
-  switch(action.type) {
+    switch (action.type) {
 
-    case 'AUTH_STATUS_CHANGE':
-        return Object.assign({}, state, {
-            ghAuthStatus: action.payload.ghAuthStatus
-        })
+        case 'AUTH_STATUS_CHANGE':
+            return Object.assign({}, state, {
+                ghAuthStatus: action.payload.ghAuthStatus
+            })
 
-    case 'ASSIGN_TOKEN':
-        return Object.assign({}, state, {
-            ghToken: action.payload.ghToken
-        })
+        case 'ASSIGN_TOKEN':
+            return Object.assign({}, state, {
+                ghToken: action.payload.ghToken
+            })
 
-    case 'LOAD_GISTS':
-        return Object.assign({}, state, {
-            gists: action.payload.gists
-        })
+        case 'LOAD_GISTS':
+            return Object.assign({}, state, {
+                gists: action.payload.gists
+            })
 
-    case 'LOAD_PROFILE':
-        lsh.setProfile(action.payload.profile)
-        return Object.assign({}, state, {
-          ghProfile: action.payload.profile
-        })
+        case 'LOAD_PROFILE':
+            lsh.setProfile(action.payload.profile)
+            return Object.assign({}, state, {
+                ghProfile: action.payload.profile
+            })
 
-    case 'UPDATE_BACKUP_GIST_ID':
-      lsh.putBackupGistId(action.payload.id)
-        return Object.assign({}, state, {
-            ghBackupGistId: action.payload.id
-		})
-	
-	case 'SET_CONTRIBUTORS': 
-		lsh.setContributors(action.payload.contributors)
-	return Object.assign({}, state, {
-		ghContributors: action.payload.contributors
-	})
-    default:
-      return state
-  }
+        case 'UPDATE_BACKUP_GIST_ID':
+            lsh.putBackupGistId(action.payload.id)
+            return Object.assign({}, state, {
+                ghBackupGistId: action.payload.id
+            })
+
+        case 'SET_CONTRIBUTORS':
+            lsh.setContributors(action.payload.contributors)
+            return Object.assign({}, state, {
+                ghContributors: action.payload.contributors
+            })
+        default:
+            return state
+    }
 }
 
 export default github
