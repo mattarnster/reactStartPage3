2105ed583300b22d9957984d8c82deec22a2ff08
Matt Arnold <mattarnster.co.uk@gmail.com>
2020-10-02 20:47:56 -0400

    WIP GitHub Serverless Auth

--- api/githubAuth.js
+++ api/githubAuth.js
@@ -1,4 +1,33 @@
-module.exports = (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', "https://ngstartpage.uk")
-    res.status(200).send(`Hello`)
-  }
+import fetch from 'node-fetch'
+
+export default (req, res) => {
+    // res.setHeader('Access-Control-Allow-Origin', "https://ngstartpage.uk")
+    var query = req.query
+
+    if (query.code){
+        var github_url = 'https://github.com/login/oauth/access_token'
+
+        var data = {
+            client_id: process.env.GITHUB_CLIENT_ID,
+            client_secret: process.env.GITHUB_CLIENT_SECRET,
+            code: query.code
+        }
+    
+        data = JSON.stringify(data)
+    
+        let response = fetch(github_url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8',
+                'Accept': 'application/json'
+            },
+            body: data
+        }).then(data => {
+            return data.json()
+        }).then(data => {
+            res.status(200).send(data)
+        })
+    } else {
+        res.status(400).send('Bad request')
+    }
+}
